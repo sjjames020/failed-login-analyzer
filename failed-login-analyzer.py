e99e3b2314cc0455dcba061e9948a945384ca288
sjjames020 <sjjames020@gmail.com>
2026-08-15 22:03:43 -0400

    Update: The script now accepts file paths!

--- failed-login-analyzer.py
+++ failed-login-analyzer.py
@@ -1,18 +1,28 @@
-def reportFailedLogins(log, threshold):
+def reportFailedLogins(file_path, threshold):
     dictionary = {}
     resultIPs = []
-    for line in log.splitlines():
-        fields = line.split()
-        print(fields)
-        IP  = fields[3]
-        status = fields[4]
-        if status == "FAILED":
-            if IP in dictionary:
-                dictionary[IP] = dictionary[IP] + 1
-            else:
-                dictionary[IP] = 1
-        print(dictionary)
-    for IP in dictionary:
-        if dictionary[IP] >= threshold:
-            resultIPs.append(IP)
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            fields = line.split()
+            print(fields)
+
+            IP = fields[3]
+            status = fields[4]
+
+            if status == "FAILED":
+                if IP in dictionary:
+                    dictionary[IP] = dictionary[IP] + 1
+                else:
+                    dictionary[IP] = 1
+
+            print(dictionary)
+
+        for IP in dictionary:
+            if dictionary[IP] >= threshold:
+                resultIPs.append(IP)
+
     return resultIPs
+
+if __name__ == "__main__":
+    print(reportFailedLogins("test.log", 3))
